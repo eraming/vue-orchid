94338f0fa042ae3522256eb3418c27a557075f6f
Era Ming <era.y.ming@gmail.com>
2020-06-13 15:16:23 -0700

    media query mods on nav

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -59,88 +59,83 @@ export default {
       //   }
       // ],
       // show: false,
-      navbarOptions: {
-                 elementId: 'main-navbar',
-                 isUsingVueRouter: true,
-                 mobileBreakpoint: 992,
-                 brandImagePath: './',
-                 brandImage: require('@/assets/images/lockup-color.png'),
-                 brandImageAltText: 'brand-image',
-                 collapseButtonImageOpen: require('@/assets/images/collapse-menu-dark.png'),
-                 collapseButtonImageClose: require('@/assets/images/times.png'),
-                 showBrandImageInMobilePopup: true,
-                 ariaLabelMainNav: 'Main Navigation',
-                 tooltipAnimationType: 'shift-away',
-                 menuOptionsLeft: [
-                   {
-                     type: 'dropdown',
-                     text: 'Why Dunder Mifflin',
-                     subMenuOptions: [
-                       {
-                         isLinkAction: true,
-                         type: "link",
-                         text: "About",
-                         subText: "Stupid corporate wet blankets. Like booze ever killed anyone.",
-                         path: "./gallery",
-                         iconLeft: '<i class="fa fa-star fa-fw"></i>'
-                       },
-                       {
-                         type: 'hr',
-                       },
-                       {
-                         type: 'link',
-                         text: 'Locations',
-                         subText: 'You\'re a presentation tool!',
-                         path: './locations',
-                       },
-                       {
-                         type: 'hr',
-                       },
-                       {
-                         type: 'link',
-                         text: 'Blog',
-                         subText: 'I enjoy having breakfast in bed. I like waking up to the smell of bacon. Sue me.',
-                         path: './blog',
-                       },
-                     ]
-                   },
-                   {
-                     type: 'link',
-                     text: 'Contact',
-                     subMenuOptions: [
-                       {
-                         type: 'link',
-                         text: 'Customer Service',
-                         path: './customer-service'
-                       },
-                       {
-                         type: 'link',
-                         text: 'Accounting',
-                         path: './accounting',
-                       },
-                       {
-                         type: 'hr',
-                       },
-                       {
-                         type: 'link',
-                         text: 'Reception',
-                         path: './reception',
-                         iconLeft: '<svg id="i-telephone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"> <path d="M3 12 C3 5 10 5 16 5 22 5 29 5 29 12 29 20 22 11 22 11 L10 11 C10 11 3 20 3 12 Z M11 14 C11 14 6 19 6 28 L26 28 C26 19 21 14 21 14 L11 14 Z" /> <circle cx="16" cy="21" r="4" /> </svg>',
-                       },
-                     ]
-                   },
-                   {
-                     type: 'link',
-                     text: 'Pricing',
-                     path: './pricing',
-                     iconRight: '<i class="fa fa-long-arrow-right fa-fw"></i>',
-                   },
-                 ]
-               }
-             }
-           }
+navbarOptions: {
+           elementId: 'main-navbar',
+           isUsingVueRouter: true,
+           mobileBreakpoint: 600,
+           brandImagePath: './',
+           brandImage: require('@/assets/images/lockup-color.png'),
+           brandImageAltText: 'brand-image',
+           collapseButtonImageOpen: require('@/assets/images/collapse-menu-dark.png'),
+           collapseButtonImageClose: require('@/assets/images/times.png'),
+           showBrandImageInMobilePopup: true,
+           ariaLabelMainNav: 'Main Navigation',
+           tooltipAnimationType: 'shift-away',
+           menuOptionsLeft: [
+             {
+               type: 'link',
+               text: 'About',
+               subMenuOptions: [
+                 {
+                   isLinkAction: true,
+                   type: "link",
+                   text: "Contact",
+                   subText: "short film",
+                   path: "./filmprojects",
+                   iconLeft: '<i class="fa fa-star fa-fw"></i>'
+                 }
 
+               ]
+             },
+             {
+               type: 'link',
+               text: 'Film Projects',
+               subMenuOptions: [
+                 {
+                   isLinkAction: true,
+                   type: "link",
+                   text: "The Catching",
+                   subText: "short film",
+                   path: "./filmprojects",
+                   iconLeft: '<i class="fa fa-star fa-fw"></i>'
+                 },
+                 {
+                   type: 'hr',
+                 },
+                 {
+                   type: 'link',
+                   text: 'Tincture',
+                   path: './tincture',
+                 }
+               ]
+             },
+             {
+               type: 'link',
+               text: 'Photography',
+               subMenuOptions: [
+                 {
+                   type: 'link',
+                   text: 'Customer Service',
+                   path: './gallery'
+                 }
+                 // {
+                 //   type: 'hr',
+                 // },
+                 // {
+                 //   type: 'link',
+                 //   text: 'Reception',
+                 //   path: './reception',
+                 //   iconLeft: '<svg id="i-telephone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"> <path d="M3 12 C3 5 10 5 16 5 22 5 29 5 29 12 29 20 22 11 22 11 L10 11 C10 11 3 20 3 12 Z M11 14 C11 14 6 19 6 28 L26 28 C26 19 21 14 21 14 L11 14 Z" /> <circle cx="16" cy="21" r="4" /> </svg>',
+                 // },
+               ]
+             }
+
+           ]
+         }
        }
+     }
+
+ }
 
 </script>
 
@@ -154,6 +149,25 @@ export default {
       background: darken(#ff3b30, 10%);
     }
   }
+
+  &__menu-options {
+    &--left {
+      color: red;
+    }
+  }
+
+  &__sub-menu-options {
+    &__option {
+
+      &__link {
+        color: black;
+        &:hover {
+
+        }
+      }
+
+}
+}
 }
 
 // @import url('https://fonts.googleapis.com/css?family=Karla');

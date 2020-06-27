48a3a3ac88ecaefcdb9d7acfa8b988e3bbc5b7d6
Era Ming <era.y.ming@gmail.com>
2020-06-27 14:59:00 -0700

    cleaned up navbar no logo

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -64,7 +64,7 @@ export default {
                  isUsingVueRouter: true,
                  mobileBreakpoint: 992,
                  brandImagePath: './',
-                 brandImage: require('@/assets/images/lockup-color.png'),
+                 // brandImage: require('@/assets/images/lockup-color.png'),
                  brandImageAltText: 'brand-image',
                  collapseButtonImageOpen: require('@/assets/images/collapse-menu-dark.png'),
                  collapseButtonImageClose: require('@/assets/images/times.png'),
@@ -74,14 +74,12 @@ export default {
                  menuOptionsLeft: [
                    {
                      type: 'dropdown',
-                     text: 'Why Dunder Mifflin',
                      subMenuOptions: [
                        {
                          isLinkAction: true,
                          type: "link",
-                         text: "About",
-                         subText: "Stupid corporate wet blankets. Like booze ever killed anyone.",
-                         path: "./gallery",
+                         text: "home",
+                         path: "./",
                          iconLeft: '<i class="fa fa-star fa-fw"></i>'
                        },
                        {
@@ -89,52 +87,35 @@ export default {
                        },
                        {
                          type: 'link',
-                         text: 'Locations',
-                         subText: 'You\'re a presentation tool!',
-                         path: './locations',
+                         text: 'photography',
+                         path: './gallery',
                        },
                        {
                          type: 'hr',
                        },
                        {
                          type: 'link',
-                         text: 'Blog',
-                         subText: 'I enjoy having breakfast in bed. I like waking up to the smell of bacon. Sue me.',
+                         text: 'about',
                          path: './blog',
                        },
                      ]
                    },
                    {
                      type: 'link',
-                     text: 'Contact',
+                     text: 'short films',
                      subMenuOptions: [
                        {
                          type: 'link',
-                         text: 'Customer Service',
-                         path: './customer-service'
+                         text: 'tincture',
+                         path: './tincture'
                        },
                        {
                          type: 'link',
-                         text: 'Accounting',
-                         path: './accounting',
-                       },
-                       {
-                         type: 'hr',
-                       },
-                       {
-                         type: 'link',
-                         text: 'Reception',
-                         path: './reception',
-                         iconLeft: '<svg id="i-telephone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"> <path d="M3 12 C3 5 10 5 16 5 22 5 29 5 29 12 29 20 22 11 22 11 L10 11 C10 11 3 20 3 12 Z M11 14 C11 14 6 19 6 28 L26 28 C26 19 21 14 21 14 L11 14 Z" /> <circle cx="16" cy="21" r="4" /> </svg>',
-                       },
+                         text: 'the catching',
+                         path: './catching',
+                       }
                      ]
-                   },
-                   {
-                     type: 'link',
-                     text: 'Pricing',
-                     path: './pricing',
-                     iconRight: '<i class="fa fa-long-arrow-right fa-fw"></i>',
-                   },
+                   }
                  ]
                }
              }
